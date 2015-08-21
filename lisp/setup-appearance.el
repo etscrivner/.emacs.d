128ad862dd938b2452aa6e64472979a52fecb4a5
Eric Scrivner <eric.t.scrivner@gmail.com>
2015-08-21 14:08:30 -0700

    More Additions

--- lisp/setup-appearance.el
+++ lisp/setup-appearance.el
@@ -5,7 +5,8 @@
 ;; (load-theme 'zerodark t)
 ;; (load-theme 'zenburn t)
 ;; (load-theme 'ujelly t)
-(load-theme 'ample t)
+;; (load-theme 'ample t)
+(load-theme 'spacegray t)
 ;; (load-theme 'cyberpunk t)
 
 ;; Set the font size
@@ -17,8 +18,15 @@
 ;;; Line numbers everywhere
 (global-linum-mode t)
 
+;; Add spacing on either side of line number
+(defadvice linum-update-window (around linum-dynamic activate)
+  (let* ((w (length (number-to-string
+                     (count-lines (point-min) (point-max)))))
+         (linum-format (concat " %" (number-to-string w) "d ")))
+    ad-do-it))
+
 ;;; Show fringes
-(fringe-mode 10)
+(fringe-mode -1)
 
 ;;; Indicate file sizes
 (size-indication-mode t)
---
+++ lisp/setup-smartparens.el
@@ -0,0 +1,10 @@
+;;; setup-smartparens.el --- Setup for smartparens
+;;;
+;;; Commentary:
+;;;   Provide any necessary additional setup for smartparens
+(require 'smartparens-config)
+
+;;; Code:
+
+(provide 'setup-smartparens)
+;;; setup-smartparens.el ends here
