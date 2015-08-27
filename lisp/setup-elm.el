75605ead4d76c034879fe371179480902169856a
Eric Scrivner <eric.t.scrivner@gmail.com>
2015-08-27 15:13:12 -0700

    Add Rust and Elm Modes

---
+++ lisp/setup-elm.el
@@ -0,0 +1,11 @@
+;;; setup-elm.el --- Setup for Elm language
+;;;
+;;; Commentary:
+;;;   Load and configure elm language mode
+;;;
+(require 'elm-mode)
+
+;;; Code:
+
+(provide 'setup-elm)
+;;; setup-elm.el ends here
---
+++ lisp/setup-rust.el
@@ -0,0 +1,6 @@
+(require 'rust-mode)
+
+(autoload 'rust-mode "rust-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+
+(provide 'setup-rust)
